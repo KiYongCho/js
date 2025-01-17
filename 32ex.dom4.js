da81ba76cd8e525ee95f4bb052439a9f24935df5
KiYongCho <relachoky@gmail.com>
2025-01-17 09:02:59 +0900

    123

---
+++ 32ex.dom4.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("#fileList button").forEach(btn => {
+    btn.addEventListener('click', e => {
+        console.log(e.target.dataset.btnId);
+        console.log(e.target.dataset.btnType);
+    });
+});
