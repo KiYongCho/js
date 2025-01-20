6c4b79f89fc06d1e16621c73171cccb077d04485
KiYongCho <relachoky@gmail.com>
2025-01-20 16:29:15 +0900

    123

--- 35ex.xhr.js
+++ 35ex.xhr.js
@@ -10,26 +10,68 @@
     }
 */
 
-// XHR 객체 생성
 const xhr = new XMLHttpRequest();
 
-// 요청 초기화
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos');
-// 요청 전송
-// GET방식 : 데이터를 요청URL 뒤에 붙여서 보냄
-// POST방식 : 데이터를 send메소드의 인자로 넣어서 보냄
-xhr.send();
+const xhrUtil = {
+    // httpMethod : 요청방식
+    // url : 요청URL
+    // payload : 요청시 전송할 데이터
+    init: (httpMethod, url, payload) => {
+
+       // GET방식 요청이라면
+       if (httpMethod.toUpperCase()=='GET') {
+           // payload가 있으면 url뒤에 데이터를 붙임
+           url = url + (payload ? payload : '');
+       }
+
+       // 요청 초기화
+       xhr.open(httpMethod, url);
+
+       // POST 또는 PUT 또는 PATCH 방식 요청이라면
+       if (httpMethod.toUpperCase()=='POST' ||
+           httpMethod.toUpperCase()=='PUT' ||
+           httpMethod.toUpperCase()=='PATCH'
+          ) {
+        // POST, PUT, PATCH 요청시에는 요청헤더를 설정
+        // 서버에 요청데이터가 JSON형식임을 알려줌        
+        xhr.setRequestHeader('content-type', 'application/json');
+       }
+
+       // 요청 전송
+       xhr.send(payload);
+    }
+};
 
 xhr.onreadystatechange = () => {
-    // console.log('요청상태코드값 : ' + xhr.readyState);
-    // 요청을 잘 보내고 응답을 잘 받으면
-    if (xhr.readyState===4 && xhr.status==200) {
+    if (xhr.readyState!==4) return false;
+    if (xhr.status==200 || xhr.status==201) {
         console.log(xhr.response);
     } else {
         console.error('에러 발생!', xhr.status, xhr.statusText);
     }
-};
+}
 
+// list : 전체 조회
+// xhrUtil.init('GET', 'https://jsonplaceholder.typicode.com/photos');
+
+// get : 한건 조회
+// xhrUtil.init('GET', 'https://jsonplaceholder.typicode.com/photos', '?id=1');
+
+// post : 한건 등록
+// xhrUtil.init('POST', 'https://jsonplaceholder.typicode.com/photos', 
+//    '{"albumId":101, "id":5001, "title":"title5001", "url":"url5001", "thumbnailUrl":"thumbnailUrl5001"}');
+
+// put : 한건 전체 수정
+//xhrUtil.init('PUT', 'https://jsonplaceholder.typicode.com/photos/1', 
+//    '{"albumId":1, "id":1, "title":"mod_title1", "url":"mod_url1", "thumbnailUrl":"mod_thumbnailUrl1"}');
+
+// patch : 한건 부분 수정
+//xhrUtil.init('PATCH', 'https://jsonplaceholder.typicode.com/photos/1', 
+//    '{"title":"pardmod_title1"}');  
+    
+// delete : 한건 삭제
+//xhrUtil.init('DELETE', 'https://jsonplaceholder.typicode.com/photos/1');        
+    
 
 
 
