39219c98c42f087b4c0a6f83f6fb16563353b224
KiYongCho <relachoky@gmail.com>
2025-01-22 11:30:43 +0900

    123

--- 37.asyncprogramming3.js
+++ 37.asyncprogramming3.js
@@ -54,6 +54,51 @@ promisePost(
 .catch(err => console.log(err)) // 프라미스 실패시 err=new Error(xhr.status)
 .finally(() => console.log('성공/실패 여부와 상관없이 수행!'))
 
+// 실습 1
+// https://jsonplaceholder.typicode.com/posts
+// Promise를 이용해서 id가 20인 게시물의 title을 'modified title'로 수정
+new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('PATCH', 'https://jsonplaceholder.typicode.com/posts/20');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.send('{"title":"modified title"}');
+    xhr.onload = () => {
+        if (xhr.status===200 || xhr.status===201) {
+            resolve(JSON.parse(xhr.response));
+        } else {
+            reject(new Error(xhr.status));
+        }
+    };
+}).then(res => console.log(res))
+.catch(err => console.log(err))
+.finally(() => console.log('성공/실패 여부와 상관없이 수행!'))
+
+// 실습 2
+// https://jsonplaceholder.typicode.com/comments
+// Promise를 이용해서 postId가 3인 모든 댓글을 조회
+new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/comments');
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.status===200) {
+            resolve(
+                JSON.parse(xhr.response)
+                .filter(comment=>comment.postId=="3")
+            );
+        } else {
+            reject(new Error(xhr.status));
+        }
+    };
+}).then(res => console.log(res))
+.catch(err => console.log(err))
+.finally(() => console.log('성공/실패 여부와 상관없이 수행!'))
+
+// 프라미스 체이닝
+const url = 'https://jsonplaceholder.typicode.com';
+
+
+
 
 
 
